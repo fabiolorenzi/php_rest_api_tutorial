046e412adb0cef4a047aa8b3f3b9a58f5ec94de5
fabiolorenzi <fabiolorenzi@gmail.com>
2021-10-28 10:34:51 +0100

    first commit

---
+++ config/Database.php
@@ -0,0 +1,22 @@
+<?php
+    class Database {
+        private $host = "localhost";
+        private $db_name = "php_rest_api";
+        private $username = "root";
+        private $password = "L3g!nord";
+        private $conn;
+
+        public function connect() {
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch(PDOExeption $e) {
+                echo "Connection Error: " . $e->getMessage();
+            };
+
+            return $this->conn;
+        }
+    }
+?>
